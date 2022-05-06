3cadcc43a80bf941282556bf716f82741ecf94a5
Kayzwer Lee <lkyu753@gmail.com>
2022-05-06 16:37:58 +0800

    .

--- RL_method1_connect4.py
+++ RL_method1_connect4.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from Connect4 import Connect4
+import pickle
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -14,22 +15,36 @@ class Network(nn.Module):
         learning_rate: float
     ) -> None:
         super(Network, self).__init__()
-        self.layers = nn.Sequential(
+        self.feature_layer = nn.Sequential(
             nn.Linear(input_dim, 128),
-            nn.Mish(),
+            nn.Mish()
+        )
+        self.advantage_layer = nn.Sequential(
             nn.Linear(128, 256),
             nn.Mish(),
             nn.Linear(256, 256),
             nn.Mish(),
+            nn.Linear(256, 256),
+            nn.Mish(),
+            nn.Linear(256, output_dim)
+        )
+        self.value_layer = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.Mish(),
             nn.Linear(256, 128),
             nn.Mish(),
-            nn.Linear(128, output_dim)
+            nn.Linear(128, 64),
+            nn.Mish(),
+            nn.Linear(64, 1)
         )
         self.optimizer = optim.RMSprop(self.parameters(), learning_rate)
         self.loss = nn.SmoothL1Loss()
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layers.forward(x.float())
+        feature = self.feature_layer.forward(x.float())
+        advantage = self.advantage_layer(feature)
+        value = self.value_layer(feature)
+        return value + advantage - advantage.mean(dim = -1, keepdim = True)
 
 
 class Epsilon_Controller:
@@ -69,10 +84,10 @@ class Replay_Buffer:
         self.cur_size = 0
         self.buffer_size = buffer_size
         self.batch_size = batch_size
-        self.state_memory = np.zeros((input_dim, batch_size), dtype = np.float32)
-        self.action_memory = np.zeros(batch_size, dtype = np.int8)
+        self.state_memory = np.zeros((buffer_size, input_dim), dtype = np.float32)
+        self.action_memory = np.zeros(buffer_size, dtype = np.int8)
         self.reward_memory = np.zeros(buffer_size, dtype = np.float32)
-        self.next_state_memory = np.zeros((input_dim, buffer_size), dtype = np.float32)
+        self.next_state_memory = np.zeros((buffer_size, input_dim), dtype = np.float32)
 
     def store(
         self,
@@ -95,10 +110,10 @@ class Replay_Buffer:
         rewards = self.reward_memory[indexes]
         next_states = self.next_state_memory[indexes]
         return dict(
-            states = states,
-            actions = actions,
-            rewards = rewards,
-            next_states = next_states
+            states = torch.tensor(states, dtype = torch.float32),
+            actions = torch.tensor(actions, dtype = torch.long),
+            rewards = torch.tensor(rewards, dtype = torch.float32),
+            next_states = torch.tensor(next_states, dtype = torch.float32)
         )
     
     def is_ready(self) -> bool:
@@ -131,24 +146,74 @@ class Agent:
     def update_target_network(self) -> None:
         self.target_network.load_state_dict(self.network.state_dict())
 
-    def choose_action_train(self, state: torch.Tensor) -> int:
+    def choose_action_train(self, state: torch.Tensor, env: Connect4) -> int:
         if np.random.random() < self.epsilon_controller.eps:
-            return np.random.choice(self.output_dim)
+            action = np.random.choice(self.output_dim)
         else:
-            return self.network.forward(torch.tensor(state)).argmax().item()
+            action = self.network.forward(torch.tensor(state)).argmax().item()
+        if env._check_valid(action):
+            return action
+        else:
+            while True:
+                action = np.random.choice(self.output_dim)
+                if env._check_valid(action):
+                    return action
 
-    def choose_action_test(self, state: torch.Tensor) -> int:
-        return self.network.forward(torch.tensor(state)).argmax().item()
+    def choose_action_test(self, state: torch.Tensor, env: Connect4) -> int:
+        action = self.network.forward(torch.tensor(state)).argmax().item()
+        if env._check_valid(action):
+            return action
+        else:
+            while True:
+                action = np.random.choice(self.output_dim)
+                if env._check_valid(action):
+                    return action
 
-    def _compute_loss(self) -> torch.Tensor:
-        pass
+    def train(self) -> torch.Tensor:
+        batch = self.replay_buffer.sample()
+        states = batch.get("states")
+        actions = batch.get("actions")
+        rewards = batch.get("rewards")
+        next_states = batch.get("next_states")
+        batch_index = range(self.replay_buffer.batch_size)
+
+
+        q_pred = self.network.forward(states)[batch_index, actions]
+        q_next_argmax_action = self.network.forward(next_states).argmax(dim = 1)
+        q_next = rewards + self.gamma * self.target_network.forward(next_states)[batch_index, q_next_argmax_action]
+        loss = self.network.loss(q_pred, q_next)
+        loss.backward()
+        self.network.optimizer.step()
+        self.update_count += 1
+        if self.update_count % self.c == 0:
+            self.update_target_network()
+        self.epsilon_controller.decay()
+        return loss
 
 
 if __name__ == "__main__":
     env = Connect4()
-    network = Network(env.state_dim, env.action_dim, 0.001)
-
-    state = env.reset()
-    result = network.forward(torch.tensor(state))
-    print(result)
-    print(result.argmax())
+    agent = Agent(
+        env.state_dim,
+        env.action_dim, 
+        0.0001, 10000, 512,
+        1.0, "0.0001", 0.001,
+        0.99, 1024
+    )
+    iteration = 1000
+    for i in range(iteration):
+        state = env.reset()
+        done = False
+        score = 0
+        loss = 0
+        while not done:
+            
+            agent.replay_buffer.store(state, action, reward, next_state)
+            score += reward
+            state = next_state
+            if agent.replay_buffer.is_ready():
+                loss = agent.train()
+        print(f"Iteration: {i + 1}, Epsilon: {agent.epsilon_controller.eps}, Loss: {loss}, Last Game Reward: {score}")
+    
+    with open("Connect 4 agent.pickle", "wb") as f:
+        pickle.dump(agent, f)
